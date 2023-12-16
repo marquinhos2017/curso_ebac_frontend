fd1f5e09b358f906b7b7d8b954deaa8a1654584d
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2023-12-16 03:52:14 -0900

    second commit

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
-/*document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('btn-buscar-cep').addEventListener('click',function(){
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-
-        xhttp.open('GET', endpoint);
-        xhttp.send();
-
-    })
-})
-*/
-
-
-
 $(document).ready(function(){
 
-
+   
         const endpoint = `https://api.github.com/users/marquinhos2017`;
-
-        /*$.ajax(endpoint).done(function(resposta){
-            console.log(resposta);
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro}, ${cidade}, ${estado}`;
-            $('#endereco').val(endereco);
-
-            setTimeout(function(){
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            },4000);
-
-            
-
-        })
-        */
-       fetch(endpoint)
+        fetch(endpoint)
         .then(function(resposta){
             return resposta.json();
-       }).then(function(json){
+        }).then(function(json){
             console.log(json);
             const profile_avatar = document.querySelector('.profile-avatar');
             const profile_name = document.querySelector('.profile-name');
@@ -59,18 +25,40 @@ $(document).ready(function(){
            public_repos_element.innerText = json.public_repos;
           
            link.href = `${json.html_url}`;
-           
-
-            
        })
-      
-    
+       .catch(function(erro){
+            alert("Ocorreu um erro ao buscar o usuario, tente novamente");
+            const profile_avatar = document.querySelector('.profile-avatar');
+            const profile_name = document.querySelector('.profile-name');
+            const profile_username = document.querySelector('.profile-username');
+            const followers_element = document.querySelector('#followers');
+            const following_element = document.querySelector('#following');
+            const public_repos_element = document.querySelector('#public_repos');
+            const link = document.querySelector('.profile-link');
 
-    $('#formulario-pedido').submit(function(evento){
-        evento.preventDefault();
-        if($('#nome').val().length == 0){
-            throw new Error('Digite o nome');
-        }
-    })
+            profile_avatar.src = "https://thumbs.dreamstime.com/b/profile-anonymous-face-icon-gray-silhouette-person-male-default-avatar-photo-placeholder-isolated-white-background-profile-107327860.jpg";
+            profile_name.innerText ="Anonimo";
+            
+            profile_username.innerText = '@'+"aleatorio";
+
+           followers_element.innerText = "X";
+           following_element.innerText = "XX";
+           public_repos_element.innerText = "XXX";
+          
+           link.href = "anonimo@gmail.com";
+        })
+        .finally(function(){
+            setTimeout(function(){
+                
+            },1000);
+        })
+     
+    
+    
+    
 })
+
+
+
+    
 
