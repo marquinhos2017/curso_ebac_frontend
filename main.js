d641273129d33a4f93fd39e2342c1d1670bd385b
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2023-12-24 17:38:30 -0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    console.log(numero1 * numero2);
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    console.log("Ola " + nome);
+    return "Ola " + nome;
+}
+multiplicacao(2, 6);
+saudacao("Marquinhos");
---
+++ main.ts
@@ -0,0 +1,12 @@
+function multiplicacao (numero1:number,numero2:number){
+    console.log(numero1*numero2);
+    return numero1*numero2;
+}
+
+function saudacao (nome:string){
+    console.log("Ola " + nome);
+    return "Ola " + nome;
+}
+
+multiplicacao(2,6);
+saudacao("Marquinhos");
